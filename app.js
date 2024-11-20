88f717a3561b3e12af04a7e604711e58a44a218b
IsmailKacem <ismailkacem.dev@gmail.com>
2024-11-20 14:04:34 +0100

    btn remove and add volume

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ btnPlayPause.forEach(btn => {
 
 
 function handlePlayAudio(e) {
-    
     const btn = e.currentTarget
-
     // on recup le parent du btn clicked
     const audioPlayer = btn.closest(".audio-player")
     console.log(audioPlayer);
-
+    
     const songAudio = audioPlayer.querySelector(".audio-player-song")
     const spanPlayPause = btn.querySelector(".spanplaypause")
     
@@ -23,4 +21,62 @@ function handlePlayAudio(e) {
         songAudio.pause()
         spanPlayPause.textContent = "play_arrow"
     }
+}
+
+
+const inputVolume = document.querySelectorAll(".volumeControl__audioplayer")
+
+inputVolume.forEach(input => {
+    input.addEventListener("input", handleVolumeControle)
+});
+
+function handleVolumeControle(e) {
+    const input = e.currentTarget
+    const audioPlayer = input.closest(".audio-player")
+    const songAudio = audioPlayer.querySelector(".audio-player-song")
+    
+    const volumeMapping = {
+        0: 0, // 0% de volume
+        1: 0.15, // 15% de volume
+        2: 0.30, // 30% de volume
+        3: 0.60, // 60% de volume
+        4: 0.80, // 80% de volume
+        5: 1.00  // 100% de volume
+    };
+
+    // converti la valeur la string en number, ex: "2" => 2
+    let volumeValue = parseInt(input.value, 10);
+    songAudio.volume = volumeMapping[volumeValue];
+    
+    console.log(`volume : ${volumeValue}`);
+    // console.log(songAudio);
+}
+
+
+const btnRemove = document.querySelectorAll(".btnremove")
+
+btnRemove.forEach(btn => {
+    btn.addEventListener("click", handleBtnRemove)
+});
+function handleBtnRemove(e){
+    const btn = e.currentTarget
+    const audioPlayer = btn.closest(".audio-player")
+    const input = audioPlayer.querySelector(".volumeControl__audioplayer")
+    input.value--
+    handleVolumeControle({currentTarget: input})
+}
+
+
+const btnAdd = document.querySelectorAll(".btnadd")
+
+btnAdd.forEach(btn => {
+    btn.addEventListener("click", handleBtnAdd)
+});
+
+function handleBtnAdd(e){
+    const btn = e.currentTarget
+    const audioPlayer = btn.closest(".audio-player")
+    const input = audioPlayer.querySelector(".volumeControl__audioplayer")
+    input.value++
+    handleVolumeControle({currentTarget: input})
 }
